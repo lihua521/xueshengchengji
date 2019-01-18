97d51526068942ea81dd94a510982cd1a4a6cb1f
李财宝 <1825306263@qq.com>
2019-01-18 10:30:05 +0800

    基本格式

--- main.c
+++ main.c
@@ -2,9 +2,41 @@
 
 int main()
 {
-   
+    struct Student www[1000];
+    int index = 0;
     printf("xueshengchengjixitong\n");
+    while (1)
+    {
+        printf("1---tianjia\n");
+        printf("2---shanchu\n");
+        printf("3---xianshi\n");
+        printf("4---yewu1\n");
+        printf("5---yewu2\n");
+        printf("6---yewu3\n");
+        printf("7---yewu4\n");
+        printf("shurugongneng\n");
 
-return 0;
+        int code;
+        if (code == 1)
+        {
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+        }
+        if (code == 6)
+        {
+        }
+        if (code == 7)
+        {
+        }
+        return 0;
     }
-  
