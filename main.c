2f364950cfd73ee9e0d0807de8f965c8c3279c2b
李财宝 <1825306263@qq.com>
2019-01-18 11:12:58 +0800

    第二部分

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-struct Student{
+struct Student
+{
     int yuwen;
     int shuxue;
     int yingyu;
@@ -20,16 +21,15 @@ int main()
         printf("6---yewu3\n");
         printf("7---yewu4\n");
         printf("shurugongneng\n");
-        
+
         int code;
-        scanf("%d",&code);
+        scanf("%d", &code);
         if (code == 1)
         {
             printf("1---shuruxingming\n");
             scanf("%s", sss[index].name);
             printf("shuruyuwenchengji\n");
             int yuwen;
-            scanf("%d", &yuwen);
             scanf("%d", &yuwen);
             sss[index].yuwen = yuwen;
             int shuxue;
@@ -45,24 +45,40 @@ int main()
             scanf("%c", &x);
         }
 
-        if (code == 2)
-        {
+        if (code == 2){
+            if (index > 0)
+            {
+                index--;
+                printf("shanchuchengong,dianjihuichejixu:\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            else
+            {
+                printf("shanchushibai,dianjihuichejixu:\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+
         }
         if (code == 3)
         {
-        }
-        if (code == 4)
-        {
-        }
-        if (code == 5)
-        {
-        }
-        if (code == 6)
-        {
-        }
-        if (code == 7)
-        {
-        }
-        return 0;
+           
     }
+    if (code == 4)
+    {
+    }
+    if (code == 5)
+    {
+    }
+    if (code == 6)
+    {
+    }
+    if (code == 7)
+    {
+    }
+    return 0;
+}
 }
